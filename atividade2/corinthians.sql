551f57df74bfbc2a9c7b364e46d1d3de21bc2393
Gabriel-Angelo-Silva <136750547+Gabriel-Angelo-Silva@users.noreply.github.com>
2024-11-02 22:42:29 -0400

    Delete atividade2 directory

--- atividade2/corinthians.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE corinthians;
-USE corinthians;
-
-CREATE TABLE contatos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    mensagem TEXT NOT NULL
-);
